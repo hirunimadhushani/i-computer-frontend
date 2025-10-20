c61171ee4bbf9f208e7a4a8ba61a33b6f12d9c84
hirunimadhushani <hirunimadushani27@gmail.com>
2025-10-20 14:11:31 +0530

    Day11

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import './App.css'
-import ProductCard from './components/productCard'
+
 
 function App() {
   
 
   return (
-    <>
-      <ProductCard
-      name="Laptop"
-				price="100,000.00"
-				image="https://picsum.photos/id/1/200/300"
-      />
+   <div className="border w-[600px] h-[600px] bg-gray-400 relative">
+    <div className= "w-[500px] h-[500px] bg-amber-200 flex flex-col items-center justify-center" >
+      <div className="w-[100px] h-[100px] bg-blue-500">
 
-      <ProductCard
-      name="Mobile"
-        price="50,000.00"
-        image="https://picsum.photos/id/2/200/300"
-      />
-      <ProductCard
-      name="Tablet"
-        price="30,000.00"
-        image="https://picsum.photos/id/3/200/300"
-      />
-   
-    </>
+      </div>
+      <div className="w-[100px] h-[100px] bg-red-600 fixed left-[520px] top-[520px]">
+
+      </div>
+
+      <div className="w-[100px] h-[100px]   bg-green-500">
+
+      </div>
+
+      <div className="w-[100px] h-[100px] bg-purple-600 absolute right-[100px] bottom-[100px]">
+
+      </div>
+
+    </div>
+
+   </div>
   )
 }
 
---
+++ src/components/userData.jsx
@@ -0,0 +1,11 @@
+export default function UserData(){
+    return(
+      
+            <div>
+                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" width="30px"/>
+                <span>Cart</span>
+                <p>Hiruni Madhushani</p>
+            </div>
+     
+    )
+}
